c4c23eeee6449e2a4805ce9a5372afa004dd9027
kopan <kopanov_03@mail.ru>
2023-04-02 22:36:17 +0700

    Добавьте файлы проекта.

---
+++ graphicsEditor_3/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace graphicsEditor_3.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ graphicsEditor_3/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace graphicsEditor_3.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
